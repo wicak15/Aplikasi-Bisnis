5673e21ed6b038e1d4a7c1e1cc68327c513519b4
Leonardo C. Surbakti <leonardosurbakti01@gmail.com>
2025-06-28 22:35:36 +0700

    action

--- app/Filament/Resources/BarangResource/RelationManagers/StocksRelationManager.php
+++ app/Filament/Resources/BarangResource/RelationManagers/StocksRelationManager.php
@@ -2,14 +2,19 @@
 
 namespace App\Filament\Resources\BarangResource\RelationManagers;
 
+use App\JenisTransaksi;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class StocksRelationManager extends RelationManager
 {
@@ -50,6 +55,37 @@ class StocksRelationManager extends RelationManager
                         blank: fn (Builder $query) => $query,
                         true: fn (Builder $query) => $query->hasBalance(),
                         false: fn (Builder $query) => $query->noBalance(),
+                    ),
+            ])
+            ->actions([
+                Tables\Actions\Action::make('masuk_stock')
+                    ->label('+ Stock')
+                    ->form([
+                        TextInput::make('keterangan')
+                            ->required(),
+                        TextInput::make('jumlah')
+                            ->label('Jumlah stock')
+                            ->required()
+                            ->minValue(1)
+                            ->numeric(),
+                    ])
+                    ->action(
+                        function (Model $record, array $data) {
+                            DB::transaction(
+                                function () use($record, $data) {
+                                    $record->update([
+                                        'balance' => $record->balance + $data['jumlah'],
+                                    ]);
+                                    $record->transaksi()->create([
+                                        'tanggal' => Carbon::today(),
+                                        'keterangan' => $data['keterangan'],
+                                        'jenis_transaksi' => JenisTransaksi::Debit,
+                                        'jumlah' => $data['jumlah'],
+                                    ]);
+                                }
+                            );
+                            
+                        }
                     ),
             ]);
     }
