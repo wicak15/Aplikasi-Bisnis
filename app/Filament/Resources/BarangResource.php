94d73a32a84f1b1eba3b2c70c0cefbf4c54fc316
Leonardo C. Surbakti <leonardosurbakti01@gmail.com>
2025-06-28 22:33:41 +0700

    Update BarangResource.php
    Action

--- app/Filament/Resources/BarangResource.php
+++ app/Filament/Resources/BarangResource.php
@@ -8,15 +8,19 @@ use App\Filament\Resources\BarangResource\RelationManagers\StocksRelationManager
 use App\Models\Barang;
 use Filament\Forms;
 use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Collection;
 
 class BarangResource extends Resource
 {
@@ -33,9 +37,9 @@ class BarangResource extends Resource
                 TextInput::make('satuan'),
                 TextInput::make('version')->readOnly(),
                 Placeholder::make('total_stock')
-                        ->visibleOn(['edit','view'])
-                        ->content(fn(Model $record)
-                        =>"{$record->totalStock}{$record->satuan}"),
+                    ->visibleOn(['edit', 'view'])
+                    ->content(fn (Model $record) 
+                    => "{$record->totalStock} {$record->satuan}"),
             ]);
     }
 
@@ -49,24 +53,88 @@ class BarangResource extends Resource
                 TextColumn::make('totalStock'),
             ])
             ->filters([
-                //
+                TernaryFilter::make('stock_balance')
+                    ->placeholder('Semua')
+                    ->trueLabel('Ada Stock')
+                    ->falseLabel('Stock Kosong')
+                    ->queries(
+                        blank: fn ($q) => $q,
+                        true: fn ($q) => $q->hasStock(),
+                        false: fn ($q) => $q->noStock(),
+                    ),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('ganti_nama_barang')
+                    ->label('Ganti Nama Barang')
+                    ->form([
+                        TextInput::make('nama')
+                            ->required(),
+                    ])
+                    ->action(
+                        function (Model $record, array $data) {
+                            $record->update([
+                                'nama' => $data['nama']
+                            ]);
+                            Notification::make()
+                                ->title("Nama Barang berhasil di update")
+                                ->info()
+                                ->send();
+                        }
+                    ),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\BulkAction::make('bulk_action')
+                        ->label('Bulk Action')
+                        ->form([
+                            TextInput::make('nama'),
+                        ])
+                        ->action(
+                            function(Collection $records, array $data) {
+                                $namas = [];
+                                foreach ($records as $record) {
+                                    $namas[] = $record->nama;
+                                }
+                                $barangDipilih = implode(', ', $namas);
+                                $nama = $data['nama'] ?? 'User';
+                                Notification::make()
+                                    ->title("{$nama} memilih: {$barangDipilih}")
+                                    ->info()
+                                    ->send();
+                            }
+                        ),
                 ]),
             ])
-            ->modifyQueryUsing(fn (Builder $query)
-                =>$query->with(['stocks']));
+            ->headerActions([
+                Tables\Actions\Action::make('header_action')
+                    ->label('Header Action')
+                    ->form([
+                        TextInput::make('nama')
+                            ->required(),
+                    ])
+                    ->action(
+                        function (array $data) {
+                            $nama = $data['nama'] ?? 'world';
+                            Notification::make()
+                                ->title("Hello {$nama}")
+                                ->info()
+                                ->send();
+                        }
+                    ),
+            ])
+            // eager load data barang beserta relasi stocks untuk
+            // mengatasi masalah N+1 query (karena totalStock dihitung menggunakan
+            // relasi stocks)
+            ->modifyQueryUsing(fn (Builder $query) 
+                => $query->with(['stocks']));
     }
 
     public static function getRelations(): array
     {
         return [
-         StocksRelationManager::class,
+            RelationManagers\StocksRelationManager::class,
         ];
     }
 
